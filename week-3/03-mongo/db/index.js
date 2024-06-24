6f97cdeb7df4dcc8914b41f8b75a91bf3bb28187
Purv Joshi <101319136+purvjoshi04@users.noreply.github.com>
2024-06-24 06:48:53 +0000

    mongodb assignment db setup and schema

--- week-3/03-mongo/db/index.js
+++ week-3/03-mongo/db/index.js
@@ -1,19 +1,29 @@
 const mongoose = require('mongoose');
+require('dotenv').config();
 
 // Connect to MongoDB
-mongoose.connect('your-mongodb-url');
+mongoose.connect(process.env.MONGODB_URL);
 
 // Define schemas
 const AdminSchema = new mongoose.Schema({
     // Schema definition here
+    Name: String,
+    userName: String,
+    password: String
+
 });
 
 const UserSchema = new mongoose.Schema({
     // Schema definition here
+    Name: String,
+    userName: String,
+    password: String
 });
 
 const CourseSchema = new mongoose.Schema({
     // Schema definition here
+    Name: String,
+    
 });
 
 const Admin = mongoose.model('Admin', AdminSchema);
